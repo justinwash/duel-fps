0c019411812cbe698eced9b3cfbb1c854c144753
Justin Wash <justin.wash@pm.me>
2020-05-06 21:08:41 -0500

    revamp player movement

--- player/player.gd
+++ player/player.gd
@@ -1,55 +1,113 @@
 extends KinematicBody
 
-const MOVE_SPEED = 8
-const MOUSE_SENS = 0.075
-# const GRAVITY = -0.98
+const GRAVITY = -24.8
+var vel = Vector3()
+const MAX_SPEED = 20
+const JUMP_SPEED = 18
+const ACCEL = 4.5
 
-onready var raycast = $RayCast
-onready var anim = $AnimationPlayer
-onready var camera = $Camera
+var dir = Vector3()
 
-onready var hud = $HUD
+const DEACCEL= 16
+const MAX_SLOPE_ANGLE = 40
+
+var camera
+var rotation_helper
+var anim
+var raycast
+var hud
+
+var MOUSE_SENSITIVITY = 0.05
 
 func _ready():
+	camera = $RotationHelper/Camera
+	rotation_helper = $RotationHelper
+	anim = $AnimationPlayer
+	raycast = $RotationHelper/RayCast
+	hud = $HUD
+	
 	if (get_tree().has_network_peer() and !is_network_master()) \
 	or (!get_tree().has_network_peer() and get_network_master() == 0):
 		for element in hud.get_children():
 			element.visible = false
-	
-func _input(event):
-	if (get_tree().has_network_peer() and is_network_master()) \
-	or (!get_tree().has_network_peer() and get_network_master() != 0):
-		if event is InputEventMouseMotion:
-			rotation_degrees.y -= MOUSE_SENS * event.relative.x
-			rotation_degrees.x -= MOUSE_SENS * event.relative.y
 
 func _physics_process(delta):
 	if (get_tree().has_network_peer() and is_network_master()) \
 	or (!get_tree().has_network_peer() and get_network_master() != 0):
-		$Camera.current = true
-		var move_vec = Vector3()
-		if Input.is_action_pressed("move_forwards"):
-			move_vec.z -= 1
-		if Input.is_action_pressed("move_backwards"):
-			move_vec.z += 1
-		if Input.is_action_pressed("move_left"):
-			move_vec.x -= 1
-		if Input.is_action_pressed("move_right"):
-			move_vec.x += 1
+		camera.current = true
+		process_input(delta)
+		process_movement(delta)
+
+func process_input(delta):
+	# walking
+	dir = Vector3()
+	var cam_xform = camera.get_global_transform()
+
+	var input_movement_vector = Vector2()
+
+	if Input.is_action_pressed("move_forwards"):
+		input_movement_vector.y += 1
+	if Input.is_action_pressed("move_backwards"):
+		input_movement_vector.y -= 1
+	if Input.is_action_pressed("move_left"):
+		input_movement_vector.x -= 1
+	if Input.is_action_pressed("move_right"):
+		input_movement_vector.x += 1
+
+	input_movement_vector = input_movement_vector.normalized()
+
+	dir += -cam_xform.basis.z * input_movement_vector.y
+	dir += cam_xform.basis.x * input_movement_vector.x
+
+	# jumping
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			vel.y = JUMP_SPEED
 			
-		# move_vec.y = GRAVITY
-		move_vec = move_vec.normalized()
-		move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
-		var _velocity = move_and_collide(move_vec * MOVE_SPEED * delta)
-	
-		if Input.is_action_just_pressed("shoot") and !anim.is_playing():
-			anim.play("shoot")
-			var coll = raycast.get_collider()
-			if raycast.is_colliding() and coll.has_method("kill"):
-				coll.kill()
-		
+	# shooting	
+	if Input.is_action_just_pressed("shoot") and !anim.is_playing():
+		anim.play("shoot")
+		var coll = raycast.get_collider()
+		if raycast.is_colliding() and coll.has_method("kill"):
+			coll.kill()
+			
 		if get_tree().has_network_peer():
 			rpc_unreliable("set_pos", global_transform)
+
+func process_movement(delta):
+	dir.y = 0
+	dir = dir.normalized()
+
+	vel.y += delta * GRAVITY
+
+	var hvel = vel
+	hvel.y = 0
+
+	var target = dir
+	target *= MAX_SPEED
+
+	var accel
+	if dir.dot(hvel) > 0:
+		accel = ACCEL
+	else:
+		accel = DEACCEL
+
+	hvel = hvel.linear_interpolate(target, accel * delta)
+	vel.x = hvel.x
+	vel.z = hvel.z
+	vel = move_and_slide(vel, Vector3(0, 1, 0), 0.05, 4, deg2rad(MAX_SLOPE_ANGLE))
+
+func _input(event):
+	if (get_tree().has_network_peer() and is_network_master()) \
+	or (!get_tree().has_network_peer() and get_network_master() != 0):
+		if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			rotation_helper.rotate_x(deg2rad(event.relative.y * MOUSE_SENSITIVITY * -1))
+			self.rotate_y(deg2rad(event.relative.x * MOUSE_SENSITIVITY * -1))
+
+			var camera_rot = rotation_helper.rotation_degrees
+			camera_rot.x = clamp(camera_rot.x, -70, 70)
+			rotation_helper.rotation_degrees = camera_rot
+
 		
 puppet func set_pos(p_pos):
 	global_transform = p_pos
