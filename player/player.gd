5d332d7e406c35d39fe297f8d07fe1eb7dfe850b
Justin Wash <justin.wash@pm.me>
2020-05-07 13:29:29 -0500

    fine-tune player movement

--- player/player.gd
+++ player/player.gd
@@ -1,43 +1,39 @@
 extends KinematicBody
 
-const GRAVITY = -24.8
-var vel = Vector3()
-const MAX_SPEED = 20
-const JUMP_SPEED = 18
-const ACCEL = 4.5
+const GRAVITY = -20
+const JUMP_SPEED = 7.5
 
-var dir = Vector3()
+const MAX_SPEED = 12
+const ACCEL = 4.5
 
 const DEACCEL= 16
 const MAX_SLOPE_ANGLE = 40
 
-var camera
-var rotation_helper
-var anim
-var raycast
-var hud
-
 var MOUSE_SENSITIVITY = 0.05
 
+var dir = Vector3()
+var vel = Vector3()
+
+onready var camera = $RotationHelper/Camera
+onready var rotation_helper = $RotationHelper
+onready var anim = $AnimationPlayer
+onready var raycast = $RotationHelper/RayCast
+onready var hud = $HUD
+
 func _ready():
-	camera = $RotationHelper/Camera
-	rotation_helper = $RotationHelper
-	anim = $AnimationPlayer
-	raycast = $RotationHelper/RayCast
-	hud = $HUD
-	
-	if (get_tree().has_network_peer() and !is_network_master()) \
-	or (!get_tree().has_network_peer() and get_network_master() == 0):
+	if is_master_or_player(0):
 		for element in hud.get_children():
 			element.visible = false
 
 func _physics_process(delta):
-	if (get_tree().has_network_peer() and is_network_master()) \
-	or (!get_tree().has_network_peer() and get_network_master() != 0):
+	if is_master_or_player(1):
 		camera.current = true
 		process_input(delta)
 		process_movement(delta)
-
+		
+		if get_tree().has_network_peer():
+			rpc_unreliable("set_pos", global_transform)
+			
 func process_input(delta):
 	# walking
 	dir = Vector3()
@@ -70,15 +66,12 @@ func process_input(delta):
 		var coll = raycast.get_collider()
 		if raycast.is_colliding() and coll.has_method("kill"):
 			coll.kill()
-			
-		if get_tree().has_network_peer():
-			rpc_unreliable("set_pos", global_transform)
 
 func process_movement(delta):
 	dir.y = 0
 	dir = dir.normalized()
 
-	vel.y += delta * GRAVITY
+	vel.y += delta * GRAVITY * 1.1
 
 	var hvel = vel
 	hvel.y = 0
@@ -98,8 +91,7 @@ func process_movement(delta):
 	vel = move_and_slide(vel, Vector3(0, 1, 0), 0.05, 4, deg2rad(MAX_SLOPE_ANGLE))
 
 func _input(event):
-	if (get_tree().has_network_peer() and is_network_master()) \
-	or (!get_tree().has_network_peer() and get_network_master() != 0):
+	if is_master_or_player(1):
 		if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 			rotation_helper.rotate_x(deg2rad(event.relative.y * MOUSE_SENSITIVITY * -1))
 			self.rotate_y(deg2rad(event.relative.x * MOUSE_SENSITIVITY * -1))
@@ -114,3 +106,10 @@ puppet func set_pos(p_pos):
 
 func kill():
 	print('hit target. do dmg')
+	
+func is_master_or_player(id):
+	if (get_tree().has_network_peer() and !is_network_master()) \
+	or (!get_tree().has_network_peer() and get_network_master() == id):
+		return true
+	else:
+		return false
